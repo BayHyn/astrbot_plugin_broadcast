4d38267903f9d95c9a90048760dab1f3f619724d
Zhalslar <zsl2936169201@gmail.com>
2025-05-04 00:21:22 +0800

    增加防误触机制

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from aiocqhttp import CQHttp
 from astrbot.api.star import Context, Star, register
 from astrbot.core.config.astrbot_config import AstrBotConfig
 from astrbot.core.message.message_event_result import MessageChain
-from astrbot.core.platform.astr_message_event import AstrMessageEvent
 from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
     AiocqhttpMessageEvent,
 )
@@ -21,7 +20,7 @@ import random
     "astrbot_plugin_broadcast",
     "Zhalslar",
     "广播助手，帮助你向所有群聊广播消息(支持任何格式消息)",
-    "1.0.0",
+    "1.0.2",
     "https://github.com/Zhalslar/astrbot_plugin_broadcast",
 )
 class NobotPlugin(Star):
@@ -29,6 +28,7 @@ class NobotPlugin(Star):
         super().__init__(context)
         self.config = config
         self.disable_gids: list[str] = config.get("disable_gids", [])
+        self.broadcast_message = None
 
     async def get_able_gids(self, client: CQHttp) -> list[str] | None:
         all_groups = await client.get_group_list()
@@ -36,16 +36,20 @@ class NobotPlugin(Star):
         able_gids = [gid for gid in all_gids if gid not in self.disable_gids]
         return able_gids
 
-    async def get_target_group(self, event: AiocqhttpMessageEvent, group_index: int | None = None):
+    async def get_target_group(
+        self, event: AiocqhttpMessageEvent, group_index: int | None = None
+    ):
         """
         获取目标群组的 ID 和名称
         """
         try:
             all_groups = await event.bot.get_group_list()
             all_groups.sort(key=lambda x: x["group_id"])
-            group_map = {str(group["group_id"]): group["group_name"] for group in all_groups}
+            group_map = {
+                str(group["group_id"]): group["group_name"] for group in all_groups
+            }
 
-            if group_index and event.is_admin(): # 仅管理员可以指定索引
+            if group_index and event.is_admin():  # 仅管理员可以指定索引
                 try:
                     target_group_id = str(all_groups[group_index - 1]["group_id"])
                     group_name = group_map[target_group_id]
@@ -63,7 +67,9 @@ class NobotPlugin(Star):
             return None, None
 
     @filter.command("开启广播")
-    async def enable_broadcast(self, event: AiocqhttpMessageEvent, group_index: int | None = None):
+    async def enable_broadcast(
+        self, event: AiocqhttpMessageEvent, group_index: int | None = None
+    ):
         """
         开启广播，开启后当前群聊可接收来自机器人管理员的广播消息。
         """
@@ -79,9 +85,10 @@ class NobotPlugin(Star):
         else:
             yield event.plain_result(f"【{group_name}】已开启广播，无需重复开启")
 
-
     @filter.command("关闭广播")
-    async def disable_broadcast(self, event: AiocqhttpMessageEvent, group_index: int | None = None):
+    async def disable_broadcast(
+        self, event: AiocqhttpMessageEvent, group_index: int | None = None
+    ):
         """
         关闭广播，关闭后当前群聊将不再接收来自机器人管理员的广播消息。
         """
@@ -97,6 +104,7 @@ class NobotPlugin(Star):
         else:
             yield event.plain_result(f"【{group_name}】已关闭广播，无需重复关闭")
 
+    @filter.permission_type(filter.PermissionType.ADMIN)
     @filter.command("广播列表")
     async def broadcast_list(self, event: AiocqhttpMessageEvent):
         """查看将要向哪些群广播"""
@@ -119,48 +127,66 @@ class NobotPlugin(Star):
 
     @filter.permission_type(filter.PermissionType.ADMIN)
     @filter.command("广播")
-    async def handle_empty_mention(self, event: AstrMessageEvent):
+    async def broadcast(self, event: AiocqhttpMessageEvent):
         """向所有启用广播的群聊广播消息"""
+        group_id = event.get_group_id()
         sender_id = event.get_sender_id()
         yield event.plain_result("请30秒内发送要广播的消息")
+
         @session_waiter(timeout=30, record_history_chains=True)  # type: ignore
         async def empty_mention_waiter(
-            controller: SessionController, event: AstrMessageEvent
+            controller: SessionController, event: AiocqhttpMessageEvent
         ):
-            if sender_id == event.get_sender_id():
-                result = event.get_result()
-                print(result)
-                if event.get_platform_name() == "aiocqhttp":
-                    from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
-                        AiocqhttpMessageEvent,
+            if group_id != event.get_group_id() or sender_id != event.get_sender_id():
+                return
+
+            client = event.bot
+            able_gids = await self.get_able_gids(client)
+            if not able_gids:
+                await event.send(event.make_result().message("没有可广播的群聊"))
+                controller.stop()
+                return
+
+            if event.message_str == "取消广播":
+                await event.send(event.make_result().message("广播已取消"))
+                controller.stop()
+                return
+
+            # 存储最后一条消息，当用户输入确认广播时发送
+            if event.message_str != "确认广播":
+                self.broadcast_message = await event._parse_onebot_json(
+                    MessageChain(chain=event.message_obj.message)
+                )
+                await event.send(
+                    event.make_result().message(
+                        f"准备向{len(able_gids)}个群广播这条消息\n请发送“确认广播/取消广播”"
                     )
+                )
+                controller.keep(timeout=30, reset_timeout=True)
 
-                    assert isinstance(event, AiocqhttpMessageEvent)
-                    client = event.bot
-                    able_gids = await self.get_able_gids(client)
-                    if able_gids:
-                        message_result = event.make_result().message(f"正在向{len(able_gids)}个群广播...")
-                        await event.send(message_result)
-                        success_count = 0  # 发送成功的群组数量
-                        failure_count = 0  # 发送失败的群组数量
-                        message_chain = MessageChain(chain=event.message_obj.message)
-                        for gid in able_gids:
-                            await asyncio.sleep(random.randint(1, 3))  # 控制发送间隔
-                            try:
-                                ob_message = await event._parse_onebot_json(message_chain)
-                                await client.send_group_msg(
-                                    group_id=int(gid), message=ob_message
-                                )
-                                success_count += 1
-                            except Exception as e:
-                                failure_count += 1
-                                logger.error(f"向群组 {gid} 发送消息失败: {e}")
-                                pass
+            elif self.broadcast_message:
+                await event.send(event.make_result().message("正在广播中..."))
+                success_count = 0  # 发送成功的群组数量
+                failure_count = 0  # 发送失败的群组数量
+                for gid in able_gids:
+                    await asyncio.sleep(random.randint(1, 3))  # 控制发送间隔
+                    try:
+                        await client.send_group_msg(
+                            group_id=int(gid), message=self.broadcast_message
+                        )
+                        success_count += 1
+                    except Exception as e:
+                        failure_count += 1
+                        logger.error(f"向群组 {gid} 发送消息失败: {e}")
+                        pass
 
-                        reply = f"广播完成\n成功：{success_count}个群\n失败：{failure_count}个群"
-                        message_result = event.make_result().message(reply)
-                        await event.send(message_result)
-                        controller.stop()
+                await event.send(
+                    event.make_result().message(
+                        f"广播完成\n成功：{success_count}个群\n失败：{failure_count}个群"
+                    )
+                )
+                self.broadcast_message = None
+                controller.stop()
 
         try:
             await empty_mention_waiter(event)
